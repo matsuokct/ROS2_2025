7a3def7c6263e46aae66bfab42c68ad3a2de3935
matsuokct <matsuo@kct.ac.jp>
2025-10-19 23:41:52 +0000

    Add week04 counter_py.py

---
+++ week4/counter_py.py
@@ -0,0 +1,43 @@
+import rclpy
+from std_msgs.msg import Int32
+from counter_python import counter_config
+
+def number_pub():
+    rclpy.init()
+
+    node = rclpy.create_node('simple_counter')
+
+    publisher = node.create_publisher(Int32, 'number_topic', 10)
+
+    counter=0
+
+    def timer_callback():
+        nonlocal counter
+
+        counter += 1
+
+        num_msg = Int32()
+        num_msg.data =counter
+
+        print('Publishing data:', num_msg.data)
+
+
+        publisher.publish(num_msg)
+
+    timer_period = 1.0
+
+    timer = node.create_timer(timer_period, timer_callback)
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        print('Node shutdown requested by user!')
+    
+    finally:
+        if rclpy.ok():
+            node.destroy_timer(timer)
+            node.destroy_node()
+            rclpy.shutdown()
+
+if __name__=='__main__':
+    number_pub()
